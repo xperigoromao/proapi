bb26e0ce7c33ac572c9a5f9e346be499dbeaec14
xperigoromao <romaocarneiro@hotmail.com>
2021-12-12 13:16:31 -0300

    primeiro commit do projeto api peolple

---
+++ src/main/java/com/roma5/proapi/ProapiApplication.java
@@ -0,0 +1,13 @@
+package com.roma5.proapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roma5/proapi/ProapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roma5.proapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
